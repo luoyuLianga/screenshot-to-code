e3fa8f4f6a3837f57054471dfff1185f7075a0d6
luoyuLianga <yuluo19851227@gmail.com>
2024-11-22 18:21:05 +0800

    调整代码

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -3,7 +3,7 @@ export const IS_RUNNING_ON_CLOUD =
   import.meta.env.VITE_IS_DEPLOYED === "true" || false;
 
 export const WS_BACKEND_URL =
-  import.meta.env.VITE_WS_BACKEND_URL || "ws://127.0.0.1:7001";
+  "ws://127.0.0.1:7001";
 
 export const HTTP_BACKEND_URL =
   import.meta.env.VITE_HTTP_BACKEND_URL || "http://127.0.0.1:7001";
